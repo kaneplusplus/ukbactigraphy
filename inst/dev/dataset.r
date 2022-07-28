aad080800e066b744426225662013f345dc049d4
Michael Kane <kaneplusplus@gmail.com>
2022-07-28 12:58:50 -0400

    Added .length method to example.

--- inst/dev/dataset.r
+++ inst/dev/dataset.r
@@ -2,7 +2,8 @@ library(devtools)
 
 document()
 
-#sdf = parquet_3f_spectral_dataset("parquet")
 ds = parquet_dir_dataset("parquet", expected_rows = 100 * 60 * 60)
 
 x = ds$.getitem(10)
+
+ds$.length()
