d7dfe2ec710459c4daeffaaecf694d8b9dbb489c
Michael Kane <kaneplusplus@gmail.com>
2022-07-28 12:54:19 -0400

    Implemented parquet directory dataset.

---
+++ R/parquet_dir_dataset.R
@@ -0,0 +1,45 @@
+#' @importFrom arrow read_parquet
+#' @importFrom torch dataset
+#' @importFrom tools file_path_as_absolute
+#' @importFrom dplyr collect
+#' @export
+ParquetDataDir = dataset(
+  name = "ParquetDataDir",
+  initialize = function(dirname, expected_rows = NULL, verbose = TRUE) {
+    self$dirname = file_path_as_absolute(dirname)
+    self$expected_rows = expected_rows
+    if (!dir.exists(dirname)) {
+      stop(paste("Directory", dirname, "does not exist."))
+    }
+    self$fns = dir(dirname)
+    if (verbose) {
+      msg = sprintf("%d files found in %s\n", length(self$fns), self$dirname)
+      cat(msg)
+    }
+  },
+  .getitem = function(index) {
+    if (index > length(self$fns) || index < 1) {
+      stop("Index out of bounds")
+    }
+    pqr = 
+      read_parquet(
+        file.path(self$dirname, self$fns[index]), 
+        as_data_frame = FALSE
+      )
+    if (!is.null(self$expected_rows)) {
+      if (nrow(pqr) < self$expected_rows) {
+        warning("Data set has fewer lines than number of expected samples.")
+      }
+      pqr = pqr[seq_len(self$expected_rows),]
+    } 
+    return(collect(pqr))
+  },
+  .length = function() {
+    length(self$fns)
+  }
+)
+
+#' @export
+parquet_dir_dataset = function(dirname, expected_rows = NULL, ...) {
+  ParquetDataDir(dirname, expected_rows)
+}
