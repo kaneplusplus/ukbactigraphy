a8773f5d877296be0704f355b1893c67bd6accd3
Michael J. Kane <kaneplusplus@gmail.com>
2023-01-09 13:58:04 -0500

    Initial commit.

---
+++ inst/day-regression/fit-model-demo-no-actigraphy/gpu-devel.sh
@@ -0,0 +1,4 @@
+salloc --mem-per-cpu=8GB --cpus-per-gpu=4 --gpus=1 --partition gpu --time=02:00:00
+
+# apptainer shell --shell /bin/bash --nv r-4.2.1-torchcuda.sif
+
---
+++ inst/day-regression/fit-model-demo-no-actigraphy/train-model.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --job-name=train-no-accel
+#SBATCH --time=2-00:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mem-per-cpu=8GB
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --partition=gpu
+#SBATCH --gpus=1
+
+# apptainer shell --shell /bin/bash --nv r-4.2.1-torchcuda.sif
+cd /home/mjk56/projects/takeda/day-regression/model-no-actigraphy
+apptainer exec --nv ../r-4.2.1-torchcuda.sif Rscript train-model.r
+
